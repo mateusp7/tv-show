77444a40318e1d2f4c05b690ce4a7b66c4db3b3e
mateusp7 <mateusdev1337@gmail.com>
2025-11-01 18:11:26 -0300

    test: add unit teste to ShowContent component

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
+    coverage: {
+      provider: 'v8' // or 'istanbul'
+    },
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
